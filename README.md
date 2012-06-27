7c62213be77d341d75c5ab2330ac6a863295084c
Gareth Murphy <gareth.cpm@gmail.com>
2012-06-28 00:54:09 +0100

    Real initial commit, including first completed function (data) and a simple gitignore.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 fhShiv
 ======
 
-A shiv for the client-side FeedHenry $fh API, created to enable faster local development of apps for the platform.
+A shiv for the client-side FeedHenry $fh API, created to enable faster local development of apps for
+the platform.
+
+Currently only the $fh.data() method is covered, although others are planned *soon*, based on my own
+needs.
---
+++ fhShiv.js
@@ -0,0 +1,97 @@
+/**
+ * @version 0.0.1
+ *
+ * @fileOverview A 'shiv' for the various FeedHenry APIs, to facilitate a much better local
+ * development workflow.
+ *
+ * @author Gareth Murphy (gareth.cpm@gmail.com)
+ */
+
+var $fh = {
+
+  /**
+   * Implementation of the $fh.data() method, which gets, sets or deletes simple key/value data to
+   * localStorage in the browser.
+   *
+   * @param {Object} config An object containing the key (required), as well as the act and val
+   * parameters when relevant.
+   * @param {Function} successCb Callback to be called on success and given a res object upon a get.
+   * @param {Function} failCb Callback to be called on failure for some reason. Note however, that
+   * because of how localStorage works, in cases where you requested a key which isn't present, a
+   * null value is returned as a success rather than being counted as a failure!
+   *
+   * @throws {Error} Errors are thrown upon a lack of sufficient arguments, a missing key or a bad act type.
+   */
+  data: function (config, successCb, failCb) {
+    // If we're not given the callbacks, blow up.
+    if (arguments.length < 3) {
+      throw new Error("You need to provide the required arguments!");
+    }
+    // Fail if no localStorage available.
+    if (!localStorage) {
+      return failCb("localStorage not supported!");
+    }
+
+    function getData(key) {
+      return localStorage.getItem(key);
+    }
+
+    function setData(key, val) {
+      localStorage.setItem(key, val);
+    }
+
+    function removeData(key) {
+      localStorage.removeItem(key);
+    }
+
+    function buildConfig(userConfig) {
+      var newConfig = {};
+
+      // Check the only mandatory config.
+      if (!userConfig.key) {
+        throw new Error("Missing key; Make sure you include the appropriate config!");
+      }
+      newConfig.key = userConfig.key;
+
+      if (userConfig.act) {
+        if (userConfig.act !== 'load' &&
+          userConfig.act !== 'save' &&
+          userConfig.act !== 'remove') {
+          throw new Error("Invalid act value; must be either 'load', 'save' or 'remove'!");
+        }
+        newConfig.act = userConfig.act;
+      } else {
+        newConfig.act = "load";
+      }
+
+      if (userConfig.act === "save") {
+        newConfig.val = "" || userConfig.val;
+      }
+
+      return newConfig;
+    }
+
+    function buildRes(key, val) {
+      return {
+        key:key,
+        val:val
+      };
+    }
+
+    config = buildConfig(config);
+
+    switch (config.act) {
+      case "load":
+        successCb(buildRes(config.key, getData(config.key)));
+        break;
+      case "save":
+        setData(config.key, config.val);
+        successCb(); // 'res' being undefined here, in accordance with actual behaviour.
+        break;
+      case "remove":
+        removeData(config.key);
+        successCb(); // TODO: Check if this actually returns undefined 'res'.
+        break;
+    }
+  }
+};
